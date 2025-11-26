67dce6bad8e43454d82f1215b47ced7ec25176fa
Luiz Felipe <teixeiraluizfelipe736@gmail.com>
2025-11-26 18:59:41 -0300

    Tela principal do médico
    Falta adicionar as outras telas para conectar com os botões.

---
+++ src/main/java/telasSistema/TelaPrincipalMedico.java
@@ -0,0 +1,86 @@
+package telasSistema;
+
+import java.awt.EventQueue;
+
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.border.EmptyBorder;
+import java.awt.Color;
+import javax.swing.JLabel;
+import java.awt.Font;
+import javax.swing.JButton;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
+import java.awt.FlowLayout;
+import java.awt.Component;
+import java.awt.Point;
+
+public class TelaPrincipalMedico extends JFrame {
+
+	private static final long serialVersionUID = 1L;
+	private JPanel contentPane;
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					TelaPrincipalMedico frame = new TelaPrincipalMedico();
+					frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	/**
+	 * Create the frame.
+	 */
+	public TelaPrincipalMedico() {
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setBounds(100, 100, 888, 500);
+		contentPane = new JPanel();
+		contentPane.setBackground(new Color(143, 222, 239));
+		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
+		setContentPane(contentPane);
+		
+		JLabel lblBemVindoMedico = new JLabel("Bem vinda(o), Dr. xxxxxxxx!!!!");
+		lblBemVindoMedico.setBounds(276, 10, 414, 35);
+		lblBemVindoMedico.setFont(new Font("Arial Black", Font.BOLD, 24));
+		contentPane.add(lblBemVindoMedico);
+		
+		JButton btnViewProntuario = new JButton("Visualizar Prontuário");
+		btnViewProntuario.setBounds(106, 174, 154, 62);
+		btnViewProntuario.setAlignmentY(0.0f);
+		btnViewProntuario.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+			}
+		});
+		contentPane.setLayout(null);
+		contentPane.add(btnViewProntuario);
+		
+		
+		JButton btnSolicitarExame = new JButton("Solicitar exame");
+		btnSolicitarExame.setAlignmentY(0.0f);
+		btnSolicitarExame.setBounds(277, 174, 154, 62);
+		contentPane.add(btnSolicitarExame);
+		
+		JButton btnEmitirAtestado = new JButton("Emitir atestado");
+		btnEmitirAtestado.setAlignmentY(0.0f);
+		btnEmitirAtestado.setBounds(452, 174, 154, 62);
+		contentPane.add(btnEmitirAtestado);
+		
+		JButton btnEmitirPrescricao = new JButton("Emitir prescrição");
+		btnEmitirPrescricao.setAlignmentY(0.0f);
+		btnEmitirPrescricao.setBounds(631, 174, 154, 62);
+		contentPane.add(btnEmitirPrescricao);
+		
+		JLabel lblOqueDeseja = new JLabel("O que deseja realizar?");
+		lblOqueDeseja.setBounds(418, 84, 112, 14);
+		contentPane.add(lblOqueDeseja);
+
+	}
+}
