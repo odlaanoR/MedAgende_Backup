44697fde365f158d9d6cffee96e6f14d9b77c7f6
Lucas Leandro <163912927+Lucas5812@users.noreply.github.com>
2025-12-04 17:53:40 -0300

    Adições ao Banco de dados
    A Tabela de "consultas" está completa e pronta para ser utilizada
    Foi criado a tabela de "especializacoes" e sua respectiva conexão com a tabela de "medicos"
    Adicionado os atributos senha, cpf e telefone a "usuarios", que estavam em falta
    algumas adições a alterações menores nas características de atributos e outras organizações de tabela

--- res/medAgendeDdl.sql
+++ res/medAgendeDdl.sql
@@ -13,6 +13,9 @@ SET time_zone = "+00:00";
 /*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
 /*!40101 SET NAMES utf8mb4 */;
 
+--
+-- Banco de dados: `medagende(testes)`
+--
 
 -- --------------------------------------------------------
 
@@ -24,7 +27,23 @@ CREATE TABLE `consultas` (
   `id_Consultas` int(11) NOT NULL,
   `Fk_Id_Paciente` int(11) NOT NULL,
   `Fk_Matricula_Medico` int(11) NOT NULL,
-  `Data_Consulta` date DEFAULT NULL
+  `Data_Consulta` date NOT NULL,
+  `Horario_Consulta` time NOT NULL,
+  `Regiao_Consulta` varchar(45) NOT NULL,
+  `Bairro_Consulta` varchar(45) NOT NULL,
+  `Cidade_Consulta` varchar(45) NOT NULL,
+  `Situacao` varchar(45) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura para tabela `especialidades`
+--
+
+CREATE TABLE `especialidades` (
+  `Id_Especialidade` int(11) NOT NULL,
+  `Nome_Especialidade` varchar(45) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 -- --------------------------------------------------------
@@ -36,7 +55,7 @@ CREATE TABLE `consultas` (
 CREATE TABLE `medico` (
   `Matricula` int(11) NOT NULL,
   `Id_Usuario` int(11) NOT NULL,
-  `Especialidade` varchar(45) NOT NULL,
+  `Especialidade` int(11) NOT NULL,
   `Crm` char(9) NOT NULL,
   `Situacao` varchar(45) NOT NULL,
   `Rqe` char(7) NOT NULL
@@ -60,20 +79,9 @@ CREATE TABLE `paciente` (
   `Plano_De_Saude` varchar(45) DEFAULT NULL,
   `CEP` char(8) DEFAULT NULL,
   `Senha` varchar(45) NOT NULL,
-  `CPF` varchar(45) NOT NULL,
+  `CPF` char(11) NOT NULL,
   `Telefone` char(11) DEFAULT NULL,
   `Estado` varchar(45) DEFAULT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
-
--- --------------------------------------------------------
-
---
--- Estrutura para tabela `telefone`
---
-
-CREATE TABLE `telefone` (
-  `Id_Dono` int(11) NOT NULL,
-  `Telefone` varchar(45) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 -- --------------------------------------------------------
@@ -85,7 +93,9 @@ CREATE TABLE `telefone` (
 CREATE TABLE `usuarios` (
   `Id_Usuario` int(11) NOT NULL,
   `Email` varchar(90) NOT NULL,
+  `Senha` varchar(15) NOT NULL,
   `Nome` varchar(90) NOT NULL,
+  `CPF` char(11) NOT NULL,
   `Data_Nasc` date DEFAULT NULL,
   `Bairro` varchar(45) DEFAULT NULL,
   `Rua` varchar(45) DEFAULT NULL,
@@ -93,7 +103,8 @@ CREATE TABLE `usuarios` (
   `Cidade` varchar(45) DEFAULT NULL,
   `Servíco` varchar(45) DEFAULT NULL,
   `Plano_De_Saude` varchar(45) DEFAULT NULL,
-  `CEP` char(8) DEFAULT NULL
+  `CEP` char(8) DEFAULT NULL,
+  `Telefone` char(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
@@ -105,38 +116,39 @@ CREATE TABLE `usuarios` (
 --
 ALTER TABLE `consultas`
   ADD PRIMARY KEY (`id_Consultas`),
-  ADD KEY `Fk_Id_Paciente` (`Fk_Id_Paciente`),
-  ADD KEY `Id_Consulta` (`Fk_Matricula_Medico`);
+  ADD UNIQUE KEY `id_Consultas` (`id_Consultas`),
+  ADD KEY `Fk_Matricula_Medico` (`Fk_Matricula_Medico`),
+  ADD KEY `Fk_Id_Paciente` (`Fk_Id_Paciente`);
+
+--
+-- Índices de tabela `especialidades`
+--
+ALTER TABLE `especialidades`
+  ADD PRIMARY KEY (`Id_Especialidade`);
 
 --
 -- Índices de tabela `medico`
 --
 ALTER TABLE `medico`
   ADD PRIMARY KEY (`Matricula`),
-  ADD KEY `Id_Usuario` (`Id_Usuario`);
+  ADD KEY `fk_especialidade` (`Especialidade`),
+  ADD KEY `medico_ibfk_1` (`Id_Usuario`);
 
 --
 -- Índices de tabela `paciente`
 --
 ALTER TABLE `paciente`
   ADD PRIMARY KEY (`Id_Paciente`),
-  ADD UNIQUE KEY `Email` (`Email`),
   ADD UNIQUE KEY `Id_Paciente` (`Id_Paciente`),
+  ADD UNIQUE KEY `Email` (`Email`),
   ADD UNIQUE KEY `CPF` (`CPF`);
-
---
--- Índices de tabela `telefone`
---
-ALTER TABLE `telefone`
-  ADD PRIMARY KEY (`Id_Dono`,`Telefone`);
 
 --
 -- Índices de tabela `usuarios`
 --
 ALTER TABLE `usuarios`
   ADD PRIMARY KEY (`Id_Usuario`),
-  ADD UNIQUE KEY `Email` (`Email`),
-  ADD UNIQUE KEY `Id_Usuario` (`Id_Usuario`);
+  ADD UNIQUE KEY `CPF` (`CPF`);
 
 --
 -- AUTO_INCREMENT para tabelas despejadas
@@ -155,12 +167,6 @@ ALTER TABLE `paciente`
   MODIFY `Id_Paciente` int(11) NOT NULL AUTO_INCREMENT;
 
 --
--- AUTO_INCREMENT de tabela `usuarios`
---
-ALTER TABLE `usuarios`
-  MODIFY `Id_Usuario` int(11) NOT NULL AUTO_INCREMENT;
-
---
 -- Restrições para tabelas despejadas
 --
 
@@ -168,22 +174,18 @@ ALTER TABLE `usuarios`
 -- Restrições para tabelas `consultas`
 --
 ALTER TABLE `consultas`
-  ADD CONSTRAINT `Id_Consulta` FOREIGN KEY (`Fk_Matricula_Medico`) REFERENCES `medico` (`Matricula`) ON DELETE NO ACTION ON UPDATE NO ACTION,
-  ADD CONSTRAINT `consultas_ibfk_1` FOREIGN KEY (`Fk_Id_Paciente`) REFERENCES `paciente` (`Id_Paciente`) ON DELETE NO ACTION ON UPDATE NO ACTION;
+  ADD CONSTRAINT `consultas_ibfk_1` FOREIGN KEY (`Fk_Matricula_Medico`) REFERENCES `medico` (`Matricula`) ON DELETE NO ACTION ON UPDATE NO ACTION,
+  ADD CONSTRAINT `consultas_ibfk_2` FOREIGN KEY (`Fk_Id_Paciente`) REFERENCES `paciente` (`Id_Paciente`) ON DELETE NO ACTION ON UPDATE NO ACTION;
 
 --
 -- Restrições para tabelas `medico`
 --
 ALTER TABLE `medico`
+  ADD CONSTRAINT `fk_especialidade` FOREIGN KEY (`Especialidade`) REFERENCES `especialidades` (`Id_Especialidade`) ON DELETE NO ACTION ON UPDATE NO ACTION,
   ADD CONSTRAINT `medico_ibfk_1` FOREIGN KEY (`Id_Usuario`) REFERENCES `usuarios` (`Id_Usuario`) ON DELETE NO ACTION ON UPDATE NO ACTION;
-
---
--- Restrições para tabelas `telefone`
---
-ALTER TABLE `telefone`
-  ADD CONSTRAINT `telefone_ibfk_1` FOREIGN KEY (`Id_Dono`) REFERENCES `usuarios` (`Id_Usuario`) ON DELETE NO ACTION ON UPDATE NO ACTION;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+
