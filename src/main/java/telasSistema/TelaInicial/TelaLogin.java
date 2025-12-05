7813d74885d7ee013bfc5ab00d1d4c2ec4807b88
HeitorSantana <151783683+HeitorSantana@users.noreply.github.com>
2025-12-04 22:57:07 -0300

    Adicionando read no telaLogin

--- src/main/java/telasSistema/TelaInicial/TelaLogin.java
+++ src/main/java/telasSistema/TelaInicial/TelaLogin.java
@@ -1,41 +1,41 @@
 package telasSistema.TelaInicial;
 
-import java.awt.BorderLayout;
+import java.awt.Color;
 import java.awt.EventQueue;
+import java.awt.Font;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
 
+import javax.swing.ButtonGroup;
+import javax.swing.JButton;
 import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JOptionPane;
 import javax.swing.JPanel;
+import javax.swing.JPasswordField;
+import javax.swing.JRadioButton;
+import javax.swing.JTextField;
 import javax.swing.border.EmptyBorder;
 
+import conexao.ConnectionFactory;
 import telasSistema.Administrador.TelaPrincipalAdministrador;
 import telasSistema.Medico.TelaPrincipalMedico;
 import telasSistema.Secretaria.TelaPrincipalSecretaria;
 
-import javax.swing.JLabel;
-import javax.swing.JTextField;
-import javax.swing.JCheckBox;
-import javax.swing.JButton;
-import java.awt.Font;
-import javax.swing.JPasswordField;
-import java.awt.Color;
-import javax.swing.JLayeredPane;
-import javax.swing.JOptionPane;
-
-import java.awt.Button;
-import java.awt.event.ActionListener;
-import java.awt.event.ActionEvent;
-import java.awt.Label;
-
-import javax.swing.ButtonGroup;
-import javax.swing.ImageIcon;
-import java.awt.Dimension;
-import javax.swing.JRadioButton;
-
 public class TelaLogin extends JFrame {
 
+	Connection connection = null;
+	PreparedStatement pst = null;
+	ResultSet rs = null;
 	private static final long serialVersionUID = 1L;
 	private JPanel contentPane;
 	private JPasswordField passwordField;
+
+	private JTextField textFieldEmail;
 
 	/**
 	 * Launch the application.
@@ -65,203 +65,172 @@ public class TelaLogin extends JFrame {
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
 		setContentPane(contentPane);
 		contentPane.setLayout(null);
-		
+
+		// Conecta ao banco
+		connection = ConnectionFactory.getConnection();
+		System.out.println(connection);
+
 		JPanel panel = new JPanel();
 		panel.setBackground(new Color(255, 255, 255));
 		panel.setBounds(357, 1, 372, 417);
 		contentPane.add(panel);
 		panel.setLayout(null);
-		
+
 		JLabel lblEmail = new JLabel("Digite aqui seu e-mail:");
 		lblEmail.setFont(new Font("Segoe UI", Font.PLAIN, 12));
-		lblEmail.setBounds(95, 94, 156, 32);
+		lblEmail.setBounds(114, 114, 156, 32);
 		panel.add(lblEmail);
-		
-		JTextField textFieldEmail = new JTextField();
-		textFieldEmail.setBounds(95, 126, 175, 19);
+
+		textFieldEmail = new JTextField();
+		textFieldEmail.setBounds(95, 143, 175, 40);
 		panel.add(textFieldEmail);
 		textFieldEmail.setColumns(10);
-		
+
 		JLabel lblSenha = new JLabel("Digite aqui sua senha:");
 		lblSenha.setFont(new Font("Segoe UI", Font.PLAIN, 12));
-		lblSenha.setBounds(95, 145, 175, 31);
+		lblSenha.setBounds(114, 194, 175, 31);
 		panel.add(lblSenha);
-		
-		JLabel lblMatricula = new JLabel("Digite aqui sua matrícula:");
-		lblMatricula.setFont(new Font("Segoe UI", Font.PLAIN, 12));
-		lblMatricula.setBounds(95, 198, 175, 15);
-		panel.add(lblMatricula);
-		
-		JTextField textFieldSenha = new JTextField();
-		textFieldSenha.setBounds(95, 224, 178, 18);
-		panel.add(textFieldSenha);
-		textFieldSenha.setColumns(10);
-		
-	//BOTOES SELECAO PROFISSIONAL
-		
-		JRadioButton rdbtnSecretaria = new JRadioButton("Secretária");
-		rdbtnSecretaria.setBounds(20, 277, 102, 20);
-		panel.add(rdbtnSecretaria);
-		
-		JRadioButton rdbtnMedico = new JRadioButton("Médico");
-		rdbtnMedico.setBounds(128, 277, 102, 20);
-		panel.add(rdbtnMedico);
-		
-		JRadioButton rdbtnAdministrador = new JRadioButton("Administrador");
-		rdbtnAdministrador.setBounds(247, 277, 102, 20);
-		panel.add(rdbtnAdministrador);
-		
-		ButtonGroup grupoBotoes= new ButtonGroup();
-		grupoBotoes.add(rdbtnSecretaria);
-		grupoBotoes.add(rdbtnMedico);
-		grupoBotoes.add(rdbtnAdministrador);
-		
-		
-		//BOTAO LOGIN: 
+
+		ButtonGroup grupoBotoes = new ButtonGroup();
+
+		// BOTAO LOGIN:
 		JButton btnLogin = new JButton("LOGIN");
 		btnLogin.addActionListener(new ActionListener() {
-			
-		
+
 			@Override
 			public void actionPerformed(ActionEvent e) {
-				// TODO Auto-generated method stub
-				//condição para não deixar ele passar se estiver em branco 
+				// condição para não deixar ele passar se estiver em branco
 				String mensagemDeErro = "Por favor, preencha os seguintes campos obrigatórios:\n";
-				int camposEmBranco=0;
-				if (textFieldEmail.getText().trim().isEmpty()) {
-			        camposEmBranco++;
-			        mensagemDeErro += "- E-mail\n"; 
-			    }
-			    
-			
-			    if (textFieldSenha.getText().trim().isEmpty()) {
-			        camposEmBranco++;
-			        mensagemDeErro += "- Matrícula\n"; 
-			    }
-			    
-			  
-			    if (passwordField.getPassword().length==0) { 
-			        camposEmBranco++;
-			        mensagemDeErro += "- Senha\n";
-			    }
+				int camposEmBranco = 0;
 
-			    
-			    if (camposEmBranco > 0) {
-			        
-			        
-			        // 
-			        JOptionPane.showMessageDialog(
-			            null,
-			            mensagemDeErro,
-			            "Erro de Validação",
-			            JOptionPane.WARNING_MESSAGE 
-			        );
-			        return; 
-			    }
-				
-				if (rdbtnSecretaria.isSelected()) {
-					TelaPrincipalSecretaria tela = new TelaPrincipalSecretaria();
-					tela.setLocationRelativeTo(null);
-					tela.setVisible(true);
-					 dispose();
+				// Agora, estas variáveis estão acessíveis!
+				if (textFieldEmail.getText().trim().isEmpty()) {
+					camposEmBranco++;
+					mensagemDeErro += "- E-mail\n";
 				}
-				
-				else if (rdbtnMedico.isSelected()){
-					TelaPrincipalMedico tela = new TelaPrincipalMedico();
-					tela.setLocationRelativeTo(null);
-					tela.setVisible(true);
-					dispose();
+
+
+				if (passwordField.getPassword().length == 0) {
+					camposEmBranco++;
+					mensagemDeErro += "- Senha\n";
 				}
-				
-				else if (rdbtnAdministrador.isSelected()) {
-					TelaPrincipalAdministrador tela = new TelaPrincipalAdministrador();
-					tela.setLocationRelativeTo(null);
-					tela.setVisible(true);
-					dispose();
+
+				if (camposEmBranco > 0) {
+					JOptionPane.showMessageDialog(null, mensagemDeErro, "Erro de Validação",
+							JOptionPane.WARNING_MESSAGE);
+					return;
 				}
-				//aqui, ficaria os outros else ifs pra medico e administrador!!
+
+				// Aqui você chamaria o método logar() e faria a verificação
+				// logar();
+				logar();
+
 			}
 		});
+
 		btnLogin.setFont(new Font("Segoe UI", Font.PLAIN, 12));
 		btnLogin.setBackground(new Color(0, 159, 255));
-		btnLogin.setBounds(149, 313, 84, 20);
+		btnLogin.setBounds(128, 303, 105, 32);
 		panel.add(btnLogin);
-		
+
 		JLabel lblLogin = new JLabel("LOGIN");
 		lblLogin.setFont(new Font("Segoe UI", Font.BOLD, 26));
-		lblLogin.setBounds(128, 11, 105, 39);
+		lblLogin.setBounds(128, 31, 105, 39);
 		panel.add(lblLogin);
-		
+
 		JLabel lblRealizarLogin = new JLabel("Realize o login para continuar");
 		lblRealizarLogin.setFont(new Font("Segoe UI", Font.PLAIN, 14));
-		lblRealizarLogin.setBounds(81, 61, 189, 19);
+		lblRealizarLogin.setBounds(95, 84, 189, 19);
 		panel.add(lblRealizarLogin);
-		
+
 		passwordField = new JPasswordField();
-		passwordField.setBounds(95, 179, 175, 18);
+		passwordField.setBounds(95, 225, 175, 39);
 		panel.add(passwordField);
-		
-		JLabel Cadastro = new JLabel("Ainda não possui cadastro no sistema?");
-		Cadastro.setFont(new Font("Segoe UI", Font.PLAIN, 12));
-		Cadastro.setBounds(20, 343, 220, 19);
-		panel.add(Cadastro);
-		
-		JButton btnCadastrar = new JButton("Cadastrar");
-		btnCadastrar.addActionListener(new ActionListener() {
-			public void actionPerformed(ActionEvent e) {
-				TelaCadastro tela = new TelaCadastro();
-				tela.setLocationRelativeTo(null);
-				tela.setVisible(true);
-				 dispose();
-			}
-		});
-		btnCadastrar.setFont(new Font("Segoe UI", Font.PLAIN, 12));
-		btnCadastrar.setBackground(new Color(255, 255, 255));
-		btnCadastrar.setBounds(236, 344, 97, 16);
-		panel.add(btnCadastrar);
-		
+
 		JLabel LogarComo = new JLabel("Logar como:");
 		LogarComo.setFont(new Font("Segoe UI", Font.PLAIN, 12));
-		LogarComo.setBounds(149, 252, 97, 19);
+		LogarComo.setBounds(136, 273, 97, 19);
 		panel.add(LogarComo);
-		
+
 		JLabel EsqueceuSenha = new JLabel("Esqueceu a senha?");
 		EsqueceuSenha.setFont(new Font("Segoe UI", Font.PLAIN, 12));
-		EsqueceuSenha.setBounds(20, 372, 210, 19);
+		EsqueceuSenha.setBounds(20, 359, 210, 32);
 		panel.add(EsqueceuSenha);
-		
+
 		JButton btnRecuperarSenha = new JButton("Recuperar Senha");
 		btnRecuperarSenha.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
 				TelaRecuperaçãoSenha tela = new TelaRecuperaçãoSenha();
 				tela.setLocationRelativeTo(null);
 				tela.setVisible(true);
-				 dispose();
-				
+				dispose();
+
 			}
 		});
 		btnRecuperarSenha.setFont(new Font("Segoe UI", Font.PLAIN, 12));
 		btnRecuperarSenha.setBackground(Color.WHITE);
-		btnRecuperarSenha.setBounds(207, 375, 126, 16);
+		btnRecuperarSenha.setBounds(207, 359, 132, 32);
 		panel.add(btnRecuperarSenha);
-		
 
 		JLabel BoasVindas = new JLabel("Bem-vindo(a) ao MedAgende!");
 		BoasVindas.setFont(new Font("Trebuchet MS", Font.BOLD, 25));
 		BoasVindas.setBounds(10, 0, 349, 68);
 		contentPane.add(BoasVindas);
-		
+
 		JLabel Gerenciamento = new JLabel("Seu sistema de gerenciamento de consultas");
 		Gerenciamento.setFont(new Font("Trebuchet MS", Font.PLAIN, 14));
 		Gerenciamento.setBounds(20, 46, 349, 39);
 		contentPane.add(Gerenciamento);
 
+	} // FECHA O CONSTRUTOR TelaLogin()
+
+	public void logar() {
+		String sql = "SELECT * FROM usuarios WHERE Email=? AND Senha=?";
+		try {
+			//pst prepara a seleção que foi feita acima e pst.setString(1, textFieldEmail.getText()); substitui os valores 
+			pst = connection.prepareStatement(sql);
+			pst.setString(1, textFieldEmail.getText());
+			String senha = new String(passwordField.getPassword());
+			pst.setString(2, senha);
 		
-		
-		
-		
+			// rs = pst.executeQuery(); =execução da query(consulta ao banco de dados o select nesse caso)
+			rs = pst.executeQuery();
+			
+			if (rs.next()) {
+				//obtem o conteudo do campo pefil
+				
+				String serviço = rs.getString(11);
+				if (serviço.equals("Secretária")){
+					JOptionPane.showMessageDialog(null, "Secretaria  cadastrada	");
+					TelaPrincipalSecretaria tela = new TelaPrincipalSecretaria();
+					tela.setLocationRelativeTo(null);
+					tela.setVisible(true);
+					dispose();
+				}else if (serviço.equals("Administrador")) {
+					TelaPrincipalAdministrador tela = new TelaPrincipalAdministrador();
+					tela.setLocationRelativeTo(null);
+					tela.setVisible(true);
+					dispose();
+				}else if (serviço.equals("Médico")) {
+					TelaPrincipalMedico tela = new TelaPrincipalMedico();
+					tela.setLocationRelativeTo(null);
+					tela.setVisible(true);
+					dispose();
+				}
+				
+			} else {
+				JOptionPane.showMessageDialog(null, "Usuario e/ou senha invalido(s)");
+			}
+			// rs.next =verifica se tem email e senha correspondente
+
+		} catch (Exception e) {
+			JOptionPane.showMessageDialog(null, e);
+
+		}
 	}
-}
+
+} //  FECHA A CLASSE TelaLogin
+
 
 
 
